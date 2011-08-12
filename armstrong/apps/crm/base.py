7fe0c4f4439cdfc6e2f7c59cef0d737c2077dacc
Travis Swicegood <development@domain51.com>
2011-08-12 16:41:40 -0500

    add in docblocks

--- armstrong/apps/crm/base.py
+++ armstrong/apps/crm/base.py
@@ -7,30 +7,74 @@ class BaseBackend(object):
 
 
 class UserBackend(BaseBackend):
+    """
+    Backend for handling user events and sending them to the CRM.
+
+    Each method receives a ``user`` representing the ``User`` model
+    that the action was performed on.  It also receives a ``payload``
+    parameter that is the ``**kwargs`` received by the signal.
+    """
+
     def created(self, user, payload):
+        """
+        Called when a new user is created
+        """
         pass
 
     def updated(self, user, payload):
+        """
+        Called when a user is updated
+        """
         pass
 
     def deleted(self, user, payload):
+        """
+        Called when a user is deleted
+        """
         pass
 
     def activated(self, user, payload):
+        """
+        Called when a new user activates their account
+
+        Only called when django-registration is being used
+        """
         pass
 
     def registered(self, user, payload):
+        """
+        Called when a new user registers for an account
+
+        Only called when django-registration is being used
+        """
         pass
 
 
 class GroupBackend(BaseBackend):
+    """
+    Backend for handling group events and sending them to the CRM.
+
+    Each method receives a ``group`` representing the ``Group`` model
+    that the action was performed on.  It also receives a ``payload``
+    parameter that is the ``**kwargs`` received by the signal.
+    """
+
     def created(self, group, payload):
+        """
+        Called when a new group is created
+        """
         pass
 
     def updated(self, group, payload):
+        """
+        Called when a group is updated
+        """
         pass
 
     def deleted(self, group, payload):
+        """
+        Called when a group is deleted
+        """
         pass
 
 
